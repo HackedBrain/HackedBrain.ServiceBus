11bbe9bd055a525fd34aefd0917836dd7ef0fb13
Drew Marsh <drub0y@hotmail.com>
2015-02-06 17:00:02 -0800

    Initial work on configuration API started.

---
+++ Azure/Configuration/MessageReceiverConfiguration.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackedBrain.ServiceBus.Core;
+using HackedBrain.ServiceBus.Core.Configuration;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace HackedBrain.ServiceBus.Azure.Configuration
+{
+    public class AzureServiceBusMessageReceiverConfigurationBuilder : BaseServiceBusConfigurationBuilder
+    {
+        private string serviceBusNamespaceAddress;
+        private TokenProvider tokenProvider;
+        private string path;
+        private IMessageBodySerializer messageBodySerializer;
+        private bool withSessions;
+
+        public AzureServiceBusMessageReceiverConfigurationBuilder(ServiceBusConfigurationBuilder serviceBusConfigurationBuilder, string serviceBusNamespaceAddress) : base(serviceBusConfigurationBuilder)
+        {
+            this.serviceBusNamespaceAddress = serviceBusNamespaceAddress;
+        }
+
+        public AzureServiceBusMessageReceiverConfigurationBuilder Path(string path)
+        {
+            this.path = path;
+            
+            return this;
+        }
+
+        public AzureServiceBusMessageReceiverConfigurationBuilder UsingTokenProvider(TokenProvider tokenProvider)
+        {
+            this.tokenProvider = tokenProvider;
+
+            return this;
+        }
+
+        public AzureServiceBusMessageReceiverConfigurationBuilder UsingSharedAccessSignature(string keyName, string sharedAccessKey)
+        {
+            return this.UsingTokenProvider(TokenProvider.CreateSharedAccessSignatureTokenProvider(keyName, sharedAccessKey));
+        }
+
+        public AzureServiceBusMessageReceiverConfigurationBuilder UsingSharedAccessSignature(string sharedAccessSignature)
+        {
+            return this.UsingTokenProvider(TokenProvider.CreateSharedAccessSignatureTokenProvider(sharedAccessSignature));
+        }
+
+        public AzureServiceBusMessageReceiverConfigurationBuilder UsingMessageBodySerializer(IMessageBodySerializer messageBodySerializer)
+        {
+            this.messageBodySerializer = messageBodySerializer;
+            
+            return this;
+        }
+
+        public AzureServiceBusMessageReceiverConfigurationBuilder UsingMessageBodySerializer<TMessageBodySerializer>() where TMessageBodySerializer : IMessageBodySerializer, new()
+        {
+            return this.UsingMessageBodySerializer(new TMessageBodySerializer());
+        }
+
+        public AzureServiceBusMessageReceiverConfigurationBuilder WithSessions(bool withSessions = true)
+        {
+            this.withSessions = true;
+            
+            return this;
+        }
+
+        protected override void Build(ServiceBusConfiguration serviceBusConfiguration)
+        {
+            MessagingFactory messagingFactory = MessagingFactory.Create(
+                this.serviceBusNamespaceAddress, 
+                new MessagingFactorySettings
+                { 
+                    TokenProvider = this.tokenProvider,
+                });
+
+            IMessageReceiver messageReceiver;
+            
+            if(this.withSessions)
+            {
+                throw new NotImplementedException("Support for configuring session based message receivers not implemented yet.");
+            }
+            else
+            {
+                MessageReceiver azureMessageReceiver = messagingFactory.CreateMessageReceiver(this.path);
+
+                messageReceiver = new ServiceBusMessageReceiverMessageReceiver(azureMessageReceiver, null);
+            }
+
+            serviceBusConfiguration.MessageReceivers.Add(messageReceiver);
+        }
+    }
+
+    public static class AzureServiceBusServiceBusConfigurationBuilderExtensions
+    {
+        public static AzureServiceBusMessageReceiverConfigurationBuilder RegisterAzureServiceBusMessageReceiver(this ServiceBusConfigurationBuilder serviceBusConfigurationBuilder, string serviceBusNamespaceAddress)
+        {
+            return new AzureServiceBusMessageReceiverConfigurationBuilder(serviceBusConfigurationBuilder, serviceBusNamespaceAddress);
+        }
+    }
+}
---
+++ Core/Configuration/ServiceBusConfiguration.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackedBrain.ServiceBus.Core.Configuration
+{
+    public class ServiceBusConfiguration
+    {
+        public ServiceBusConfiguration()
+        {
+            this.MessageReceivers = new List<IMessageReceiver>();
+            this.MessageSenders = new List<IMessageSender>();
+        }
+
+        public List<IMessageReceiver> MessageReceivers
+        {
+            get;
+            private set;
+        }
+
+        public List<IMessageSender> MessageSenders
+        {
+            get;
+            private set;
+        }
+    }
+
+    public class ServiceBusConfigurationBuilder
+    {
+        List<Action<ServiceBusConfiguration>> builderCallbacks = new List<Action<ServiceBusConfiguration>>();
+
+        public void RegisterBuilderCallback(Action<ServiceBusConfiguration> builderCallback)
+        {
+            builderCallbacks.Add(builderCallback);
+        }
+
+        public ServiceBusConfiguration Build()
+        {
+            ServiceBusConfiguration result = new ServiceBusConfiguration();
+            
+            foreach(Action<ServiceBusConfiguration> builderCallback in this.builderCallbacks)
+            {
+                builderCallback(result);
+            }
+
+            return result;
+        }
+    }
+
+    public abstract class BaseServiceBusConfigurationBuilder
+    {
+        protected BaseServiceBusConfigurationBuilder(ServiceBusConfigurationBuilder serviceBusConfigurationBuilder)
+        {
+            serviceBusConfigurationBuilder.RegisterBuilderCallback(this.Build);
+        }
+
+        protected abstract void Build(ServiceBusConfiguration serviceBusConfiguration);
+    }
+}
